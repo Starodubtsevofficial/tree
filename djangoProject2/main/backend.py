d3332954b7a97c288a778f49a2d8e3427b4003a5
Gleb Starodubtsev <Starodubtsevofficial@gmail.com>
2023-09-03 08:35:39 +0300

    add response

--- djangoProject2/main/backend.py
+++ djangoProject2/main/backend.py
@@ -28,12 +28,18 @@ GetSearch = read_sqlite_table()
 print("Запрос:", GetSearch)
 
 
+
 search =  GetSearch
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
 URL     = "https://www.google.com/search?q=" + search
 print("Ссылка:", URL)
 result = requests.get(URL, headers=headers)
 
+response = requests.post(URL)
+print("Вермя запроса:", response.elapsed.total_seconds())
+
+
+#
 soup = BeautifulSoup(result.content, 'html.parser')
 
 total_results_text = soup.find("div", {"id": "result-stats"}).find(string=True, recursive=False)
@@ -59,16 +65,16 @@ def update_sqlite_table():
             sqlite_connection.close()
             print("Соединение с SQLite закрыто")
 update_sqlite_table()
-
-
+#
+#
 def update_sqlite_table_result():
     try:
         sqlite_connection = sqlite3.connect('../db.sqlite3')
         cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite для обновления количества запросов")
+        print("Подключен к SQLite для обновления количества и вермени запросоа")
         data = results_num
         sql_update_query = "UPDATE main_searchresultgooglenumber SET SearchResultGoogleNumbers=?, SearchResultGoogleSearchTime=? WHERE id=1;"
-        cursor.execute(sql_update_query, (data, 1))
+        cursor.execute(sql_update_query, (data, response.elapsed.total_seconds()))
         sqlite_connection.commit()
         print("Запись успешно обновлена в таблице main_searchresultgooglenumber ", cursor.rowcount)
         cursor.close()
