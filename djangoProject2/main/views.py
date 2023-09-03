05ef1e3c111fd035f4baeb743cbcc2fb75224dae
Gleb Starodubtsev <Starodubtsevofficial@gmail.com>
2023-09-03 17:30:13 +0300

    new funcs views.py

--- djangoProject2/main/views.py
+++ djangoProject2/main/views.py
@@ -22,25 +22,47 @@ def index(request):
             last_request = SearchWorkNumberOfSearch[len(SearchWorkNumberOfSearch)-1]
             search =  last_request
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
-            URL = "https://www.google.com/search?q=" + str(search)
-            result = requests.get(URL, headers=headers)
-            response = requests.post(URL)
-            soup = BeautifulSoup(result.content, 'html.parser')
+            URL_Google = "https://www.google.com/search?q=" + str(search)
+            URL_Yandex = "https://yandex.ru/search/?text=" + str(search)
+            URL_Yahoo  = "https://search.yahoo.com/search?p=" + str(search)
+            URL_Bing   = "https://www.bing.com/search?q=" + str(search)
+            result_google = requests.get(URL_Google, headers=headers)
+            response_google = requests.post(URL_Google)
+            ###print(soup.prettify())
+            soup = BeautifulSoup(result_google.content, 'html.parser')
             total_results_text = soup.find("div", {"id": "result-stats"}).find(string=True, recursive=False)
             results_num = ''.join([num for num in total_results_text if num.isdigit()])
+
 
             SearchResultGoogleNumber.objects.all().delete()
             SearchResultGoogleLink.objects.all().delete()
 
             SearchResultGoogleLink.objects.create(
-                SearchResultGoogleLinks = URL,
+                SearchResultGoogleLinks = URL_Google,
                 SearchResultGoogleLinksNumber = 100,
                 SearchResultGoogleLinksNumberResponse = 1
             )
+            SearchResultGoogleLink.objects.create(
+                SearchResultGoogleLinks = URL_Yandex,
+                SearchResultGoogleLinksNumber = 101,
+                SearchResultGoogleLinksNumberResponse = 2
+            )
+            SearchResultGoogleLink.objects.create(
+                SearchResultGoogleLinks = URL_Yahoo,
+                SearchResultGoogleLinksNumber = 102,
+                SearchResultGoogleLinksNumberResponse = 3
+            )
+            SearchResultGoogleLink.objects.create(
+                SearchResultGoogleLinks = URL_Bing,
+                SearchResultGoogleLinksNumber = 103,
+                SearchResultGoogleLinksNumberResponse = 4
+            )
             SearchResultGoogleNumber.objects.create(
                 SearchResultGoogleNumbers = results_num,
-                SearchResultGoogleSearchTime = response.elapsed.total_seconds()
+                SearchResultGoogleSearchTime = response_google.elapsed.total_seconds()
             )
+
+
         else: 
             error = 'Error'
 
