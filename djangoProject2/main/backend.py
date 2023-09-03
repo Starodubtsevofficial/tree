8391eb16bdb00fb2bf8983b9b3f09cc5e8d08bed
Gleb Starodubtsev <Starodubtsevofficial@gmail.com>
2023-09-03 09:59:03 +0300

    add comment

--- djangoProject2/main/backend.py
+++ djangoProject2/main/backend.py
@@ -1,7 +1,8 @@
 import requests
 import sqlite3
-from sqlite3 import Error
 from bs4 import BeautifulSoup
+
+###Выгрузка запроса
 def read_sqlite_table():
     try:
         sqlite_connection = sqlite3.connect('../db.sqlite3')
@@ -28,24 +29,25 @@ GetSearch = read_sqlite_table()
 print("Запрос:", GetSearch)
 
 
-
+### Формирование поискового запроса
 search =  GetSearch
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
 URL     = "https://www.google.com/search?q=" + search
 print("Ссылка:", URL)
 result = requests.get(URL, headers=headers)
 
+### Получение отклика запроса
 response = requests.post(URL)
 print("Вермя запроса:", response.elapsed.total_seconds())
 
 
-#
+### Получение result-stats из Google
 soup = BeautifulSoup(result.content, 'html.parser')
-
 total_results_text = soup.find("div", {"id": "result-stats"}).find(string=True, recursive=False)
 results_num = ''.join([num for num in total_results_text if num.isdigit()])
 print("Количество запросов:", results_num)
 
+### Обновление ссылки в БД
 def update_sqlite_table():
     try:
         sqlite_connection = sqlite3.connect('../db.sqlite3')
@@ -65,8 +67,8 @@ def update_sqlite_table():
             sqlite_connection.close()
             print("Соединение с SQLite закрыто")
 update_sqlite_table()
-#
-#
+
+### Обновление количества запросов и времени
 def update_sqlite_table_result():
     try:
         sqlite_connection = sqlite3.connect('../db.sqlite3')
